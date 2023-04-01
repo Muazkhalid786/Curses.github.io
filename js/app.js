3d1fca6fa99a4fda7d1d76779d568fb9bf9b32ea
Muazkhalid786 <itzmajo786@gmail.com>
2023-04-01 14:51:29 +0500

    Final Web

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-console.log("Hello")
-
 var typed = new Typed('#course', {
     strings: ['C++ <img class="langImg"  src="../img/cpp.png" >', '', 'Python <img class="langImg"  src="../img/python.png" >','C# <img class="langImg"  src="../img/csharp.png" >', 'JavaScript   <img class="langImg"  src="../img/javascript.png" >  ', 'TypeScript    <img class="langImg"  src="../img/typescript.png" >  ',  'Microsoft Powerpoint  <img class="langImg"  src="../img/powerpoint.png" > '],
     smartBackspace: true, // Default value
